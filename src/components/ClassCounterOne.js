91986dd106b332d79c06848f8642b99f71885b49
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-23 20:19:25 +0700

    Conditionally run effects

--- src/components/ClassCounterOne.js
+++ src/components/ClassCounterOne.js
@@ -6,6 +6,7 @@ class ClassCounterOne extends Component {
 
     this.state = {
       count: 0,
+      name: "",
     };
   }
 
@@ -14,12 +15,24 @@ class ClassCounterOne extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    document.title = `Clicked ${this.state.count} times`;
+    if (prevState.count !== this.state.count) {
+      console.log("Updating document title");
+
+      document.title = `Clicked ${this.state.count} times`;
+    }
   }
 
   render() {
     return (
       <div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={(e) => {
+            this.setState({ name: e.target.value });
+          }}
+        />
+
         <button onClick={() => this.setState({ count: this.state.count + 1 })}>
           Click {this.state.count} times
         </button>
