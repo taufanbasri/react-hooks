696f78f208c6a35ecd0ef4d6c012ef3ce8c648a5
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-24 06:48:31 +0700

    Get data list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 
-import IntervalClassCounter from "./components/IntervalClassCounter";
-import IntervalHookCounter from "./components/IntervalHookCounter";
+import DataFetching from "./components/DataFetching";
 
 function App() {
   return (
     <div className="App">
-      <IntervalClassCounter />
-      <IntervalHookCounter />
+      <DataFetching />
     </div>
   );
 }
