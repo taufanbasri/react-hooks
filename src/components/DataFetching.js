288a371f67679293e9f2d501cd97426e6932c951
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-24 07:11:15 +0700

    Fetch data with click event

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -4,19 +4,27 @@ import React, { useEffect, useState } from "react";
 function DataFetching() {
   const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
+
+  const handleClick = () => {
+    setIdFromButtonClick(id);
+  };
 
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then((res) => {
         setPost(res.data);
       })
       .catch((err) => console.log(err));
-  }, [id]);
+  }, [idFromButtonClick]);
 
   return (
     <div>
       <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>
+        Fetch Post
+      </button>
       <div>{post.title}</div>
     </div>
   );
