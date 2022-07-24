8e659a5a087aa76f347c3dc0d8c3e046ca4db87f
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-24 07:06:41 +0700

    Get details data

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -2,24 +2,22 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 function DataFetching() {
-  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(1);
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((res) => {
-        setPosts(res.data);
+        setPost(res.data);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [id]);
 
   return (
     <div>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <div>{post.title}</div>
     </div>
   );
 }
